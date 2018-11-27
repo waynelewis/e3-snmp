efd6af6a0c93e85612a7dbf76a7b5f8c288b3951
Wayne Lewis <wayne.lewis@esss.se>
2018-11-27 01:08:28 +0100

    Update Raritan startup script snippet to use expanded installed database

--- cmds/raritan-px3-5190r.cmd
+++ cmds/raritan-px3-5190r.cmd
@@ -1,5 +1,7 @@
 
-epicsEnvSet("MIBDIRS", "+$(SNMP_CMD_TOP)/../mibs")
+#epicsEnvSet("DB_TOP", "$(SNMP_CMD_TOP)/../template/")
+epicsEnvSet("MIBDIRS", "+$(SNMP_CMD_TOP)/../mibs/")
 devSnmpSetSnmpVersion("PDU_IP","SNMP_VERSION_2c")
-dbLoadRecords("raritan-PX3-5190R-ess.db", "PREFIX=$(IOC):, PDU_IP=$(PDU_IP)")
+#dbLoadTemplate("$(DB_TOP)/raritan-PX3-5190R-ess.substitutions", "PREFIX=$(IOC):, PDU_IP=$(PDU_IP),RACK=$(RACK),PDU=$(PDU)")
+dbLoadRecords("raritan-PX3-5190R-ess.db", "PREFIX=$(IOC):, PDU_IP=$(PDU_IP),RACK=$(RACK),PDU=$(PDU)")
 
